651500cc077d9319a47cb8a5e5296bb939a61087
seungwonleee <seungwon.code@gmail.com>
2021-05-12 22:21:22 +0900

    Fix: profile access restrictions

--- front/pages/profile.js
+++ front/pages/profile.js
@@ -1,13 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Head from 'next/head';
 import { useSelector } from 'react-redux';
 import AppLayout from '../components/AppLayout';
 import NicknameEditForm from '../components/NicknameEditForm';
 import FollowList from '../components/FollowList';
-// import Link from "next/link";
+import { useRouter } from 'next/router';
 
 const Profile = () => {
+  const router = useRouter();
   const { me } = useSelector((state) => state.user);
+
+  useEffect(() => {
+    if (!(me && me.id)) {
+      router.push('/');
+    }
+  }, [me && me.id]);
+  if (!me) {
+    return null;
+  }
   return (
     <>
       <Head>
