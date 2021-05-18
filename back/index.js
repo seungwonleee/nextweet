367d7c7b103a8037c6d4bdbf9ddff7393cea8a80
seungwonleee <seungwon.code@gmail.com>
2021-05-18 21:06:30 +0900

    Add: mysql sequelize connect

---
+++ back/index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+
+app.get("/api/post", (req, res) => {
+  res.json([
+    { id: 1, content: "post content1" },
+    { id: 2, content: "post content2" },
+    { id: 3, content: "post content3" },
+  ]);
+});
+
+app.listen(5000, () => {
+  console.log("server running...");
+});
